8c3d274a400ee80b4066cc3215945f3dbbf2c637
MaggieWelch <160075320+MaggieWelch@users.noreply.github.com>
2024-09-18 16:29:11 -0400

    Update Caroline project code.R

--- Caroline project code.R
+++ Caroline project code.R
@@ -1,12 +1,16 @@
 #create histogram of number of songs per year
 Swift$date <- as.Date(Swift$date, format = "%Y-%m-%d")
 Swift$year <- format(Swift$date, "%Y")
-dev.new() 
+
+dev.new()
 par(mar = c(10, 4, 4, 2) + 0.1, cex.axis = 0.6)
-hist(as.numeric(Swift$year),
-     breaks = seq(min(as.numeric(Swift$year)), max(as.numeric(Swift$year)), by = 1),
-     xlab = "Year",
-     ylab = "Count",
-     main = "Number of Occurrences per Year",
-     xaxt = "n")
+
+hist(as.numeric(Swift$year), 
+     breaks = seq(min(as.numeric(Swift$year)), max(as.numeric(Swift$year)), by = 1), 
+     xlab = "Year", 
+     ylab = "Count", 
+     main = "Number of Occurrences per Year", 
+     xaxt = "n", 
+     col = "lightblue")  # This sets the bar color to light blue
+
 axis(1, at = unique(as.numeric(Swift$year)), labels = unique(Swift$year), las = 2)
